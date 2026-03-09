e92b9b6483cd83a7c3fa5624e84c9d7ea7a4dad0
remyaresmi92-pixel <remyaresmi92@gmail.com>
2026-03-09 14:16:07 +0530

    Nav bar alligned

--- Blood Donating System/src/Components/Common/Nav.jsx
+++ Blood Donating System/src/Components/Common/Nav.jsx
@@ -1,47 +1,50 @@
-import React from 'react'
-import "../Styles/Nav.css"
+import React from 'react';
+import "../Styles/Nav.css";
+import { BiSolidDonateBlood } from "react-icons/bi";
+
 function Nav() {
   return (
-    <div><nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <img src="" alt="" />
-    <a class="navbar-brand" href="#">Give Blood</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">About us</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Contact</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Blood Availblility
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">A+</a></li>
-            <li><a class="dropdown-item" href="#">B+</a></li>
-            <li><a class="dropdown-item" href="#">0+</a></li>
-            <li><a class="dropdown-item" href="#">AB+</a></li>
-            <li><a class="dropdown-item" href="#">A-</a></li>
-            <li><a class="dropdown-item" href="#">B-</a></li>
-            <li><a class="dropdown-item" href="#">O-</a></li>
-            <li><a class="dropdown-item" href="#">AB-</a></li>
-          </ul>
-        </li>
-        
-      </ul>
-      
+    <div className='nav-bg'>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          {/* Brand Icon and Text */}
+          <div className="navbar-brand d-flex align-items-center">
+            <BiSolidDonateBlood className='bloodicon'  />
+            <a>Give Blood</a>
+          </div>
+
+          {/* Hamburger Toggle */}
+          <button 
+            className="navbar-toggler" 
+            type="button" 
+            data-bs-toggle="collapse" 
+            data-bs-target="#navbarSupportedContent" 
+            aria-controls="navbarSupportedContent" 
+            aria-expanded="false" 
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          {/* Navigation Links */}
+          <div className="collapse navbar-collapse nav-allign" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0"> 
+              {/* Changed me-auto to ms-auto to push links to the right if Nav-allign doesn't do it */}
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/about">About us</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
-  </div>
-</nav></div>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
